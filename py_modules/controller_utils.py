6267f306a9b5a41ccbab069fb8e7c2b9fe2178ac
fewtarius <fewtarius@steamfork.org>
2024-08-30 10:56:34 +0000

    Adjust switching so default restarts inputplumber to flush state if state exists.

--- py_modules/controller_utils.py
+++ py_modules/controller_utils.py
@@ -1,9 +1,12 @@
 import decky_plugin
 import time
+import os
 import subprocess
 import file_timeout
 import plugin_settings as settings
 import plugin_enums
+
+STATE_FILE = "/tmp/.deckyplumber.state"
 
 # sync the state of the controller to the values in settings.json
 def sync_controller_settings(current_game_id):
@@ -29,6 +32,13 @@ def execute_mode_change(mode):
     if mode == plugin_enums.ControllerModes.DEFAULT.value:
         # handle for default
         decky_plugin.logger.info(f'handle for setting default mode')
+        if os.path.exists(STATE_FILE):
+            os.remove(STATE_FILE)
+            cmd = f'systemctl restart inputplumber'
+            subprocess.run(cmd, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
         cmd = f'busctl call org.shadowblip.InputPlumber /org/shadowblip/InputPlumber/CompositeDevice0 org.shadowblip.Input.CompositeDevice SetTargetDevices "as" 1 "{mode}"'
         subprocess.run(cmd, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        with open (STATE_FILE,'w') as state:
+            state.write(f'{mode}')
+            state.close
