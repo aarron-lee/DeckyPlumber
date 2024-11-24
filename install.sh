85286ac1fd14bf8b4698c147317f9033c581b244
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-11-24 02:27:28 -0500

    enable installing older versions via install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 # does the following:
 # - controller config via DeckyPlumber Decky Plugin
+VERSION=${VERSION_TAG:-"LATEST"}
+
 if [ "$EUID" -eq 0 ]
   then echo "Please do not run as root"
   exit
@@ -13,8 +15,14 @@ cd $HOME
 sudo rm -rf $HOME/homebrew/plugins/DeckyPlumber
 
 echo "installing DeckyPlumber plugin for control configs"
+
+FINAL_URL='https://api.github.com/repos/aarron-lee/DeckyPlumber/releases/latest'
+if [ $VERSION != "LATEST" ] ; then
+  FINAL_URL="https://api.github.com/repos/aarron-lee/DeckyPlumber/releases/tags/${VERSION}"
+fi
+
 # download + install plugin
-curl -L $(curl -s https://api.github.com/repos/aarron-lee/DeckyPlumber/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/DeckyPlumber.tar.gz
+curl -L $(curl -s $FINAL_URL | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/DeckyPlumber.tar.gz
 sudo tar -xzf DeckyPlumber.tar.gz -C $HOME/homebrew/plugins
 
 # install complete, remove build dir
