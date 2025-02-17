de13a3ea398e8d887019891393f2cb37aefb86c3
honjow <honjow311@gmail.com>
2025-02-17 21:21:20 +0800

    Update the icon so that it can be displayed properly when the plugin is selected.

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { getInitialLoading } from "./redux-modules/uiSlice";
 import { setInitialState } from "./redux-modules/extraActions";
 import { Provider, useSelector } from "react-redux";
 import { currentGameIdListener } from "./backend/currentGameIdListener";
-import logo from "../assets/Icon.png";
+import { IoGameController } from "react-icons/io5";
 import ErrorBoundary from "./components/ErrorBoundary";
 import OtaUpdates from "./components/OtaUpdates";
 import { suspendResumeListeners } from "./redux-modules/steamListeners";
@@ -55,16 +55,7 @@ export default definePlugin(() => {
   return {
     title: <div className={staticClasses.Title}>DeckyPlumber</div>,
     content: <AppContainer />,
-    icon: (
-      <img
-        src={logo}
-        style={{
-          width: "1rem",
-          filter:
-            "invert(100%) sepia(0%) saturate(2%) hue-rotate(157deg) brightness(107%) contrast(101%)",
-        }}
-      />
-    ),
+    icon: <IoGameController />,
     onDismount() {
       clearListener();
       unsubscribeSuspendListeners();
